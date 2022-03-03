4ff60bc203b3b66f287146c5b2569ff37da466cd
Mathias RICHARD <E21B242Y@nfsrootha0.ensinfo.sciences.univ-nantes.prive>
2022-03-03 09:54:03 +0100

    Création Controller Crypto

---
+++ MiniProjet/src/Controller/CryptoController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CryptoController extends AbstractController
+{
+    /**
+     * @Route("/crypto", name="app_crypto")
+     */
+    public function index(): Response
+    {
+        return $this->render('crypto/index.html.twig', [
+            'controller_name' => 'CryptoController',
+        ]);
+    }
+}
