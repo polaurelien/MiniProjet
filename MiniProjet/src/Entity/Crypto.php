183066b7714f6fbd187dab3153b0f649c6803263
Pol-Aurélien <p.a.carsin@orange.fr>
2022-03-03 08:39:04 +0100

    y

---
+++ MiniProjet/src/Entity/Crypto.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CryptoRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CryptoRepository::class)
+ */
+class Crypto
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $dateCrea;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $equiUSD;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $algo;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $qteEmise;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $qteMax;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=User::class, mappedBy="Crypto")
+     */
+    private $users;
+
+    public function __construct()
+    {
+        $this->users = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDateCrea(): ?\DateTimeInterface
+    {
+        return $this->dateCrea;
+    }
+
+    public function setDateCrea(\DateTimeInterface $dateCrea): self
+    {
+        $this->dateCrea = $dateCrea;
+
+        return $this;
+    }
+
+    public function getEquiUSD(): ?int
+    {
+        return $this->equiUSD;
+    }
+
+    public function setEquiUSD(int $equiUSD): self
+    {
+        $this->equiUSD = $equiUSD;
+
+        return $this;
+    }
+
+    public function getAlgo(): ?string
+    {
+        return $this->algo;
+    }
+
+    public function setAlgo(string $algo): self
+    {
+        $this->algo = $algo;
+
+        return $this;
+    }
+
+    public function getQteEmise(): ?int
+    {
+        return $this->qteEmise;
+    }
+
+    public function setQteEmise(int $qteEmise): self
+    {
+        $this->qteEmise = $qteEmise;
+
+        return $this;
+    }
+
+    public function getQteMax(): ?string
+    {
+        return $this->qteMax;
+    }
+
+    public function setQteMax(string $qteMax): self
+    {
+        $this->qteMax = $qteMax;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, User>
+     */
+    public function getUsers(): Collection
+    {
+        return $this->users;
+    }
+
+    public function addUser(User $user): self
+    {
+        if (!$this->users->contains($user)) {
+            $this->users[] = $user;
+            $user->addCrypto($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUser(User $user): self
+    {
+        if ($this->users->removeElement($user)) {
+            $user->removeCrypto($this);
+        }
+
+        return $this;
+    }
+}
---
+++ MiniProjet/src/Entity/User.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @ORM\Table(name="`user`")
+ */
+class User
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Nom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Prenom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $mail;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $role;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Crypto::class, inversedBy="users")
+     */
+    private $Crypto;
+
+    public function __construct()
+    {
+        $this->Crypto = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->Nom;
+    }
+
+    public function setNom(string $Nom): self
+    {
+        $this->Nom = $Nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->Prenom;
+    }
+
+    public function setPrenom(string $Prenom): self
+    {
+        $this->Prenom = $Prenom;
+
+        return $this;
+    }
+
+    public function getMail(): ?string
+    {
+        return $this->mail;
+    }
+
+    public function setMail(string $mail): self
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+
+    public function getRole(): ?string
+    {
+        return $this->role;
+    }
+
+    public function setRole(string $role): self
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+
+
+
+    /**
+     * @return Collection<int, Crypto>
+     */
+    public function getCrypto(): Collection
+    {
+        return $this->Crypto;
+    }
+
+    public function addCrypto(Crypto $crypto): self
+    {
+        if (!$this->Crypto->contains($crypto)) {
+            $this->Crypto[] = $crypto;
+        }
+
+        return $this;
+    }
+
+    public function removeCrypto(Crypto $crypto): self
+    {
+        $this->Crypto->removeElement($crypto);
+
+        return $this;
+    }
+
+    // METHODES MANUELLES ----------------------------------------------------------------------------------------------
+
+    /*
+     *
+     */
+    public function setAdmin(): self
+    {
+        $this->role = "Admin";
+        return $this;
+    }
+
+    public function setUser(): self
+    {
+        $this->role = "User";
+        return $this;
+    }
+
+    public function isAdmin(): bool
+    {
+        return ($this->role == "Admin");
+    }
+}
