e6ca0c9d40ac86a6522d5bd245c9337ca7133f9c
Pol-Aurélien <p.a.carsin@orange.fr>
2022-03-27 17:09:39 +0200

    Tri par colonnes

---
+++ MiniProjet/src/Form/CryptoType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Crypto;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CryptoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('dateCrea')
+            ->add('equiUSD')
+            ->add('algo')
+            ->add('qteEmise')
+            ->add('qteMax')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Crypto::class,
+        ]);
+    }
+}
